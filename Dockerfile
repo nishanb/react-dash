5bdfde36564299ccdba4a18923b1d153608417f4
Nishan <nishanbeleri@gmail.com>
2021-08-23 00:26:24 +0530

    Add port info

--- Dockerfile
+++ Dockerfile
@@ -12,5 +12,7 @@ RUN npm run build
 #RUN Phase ( specifying from refers to a new stage terminates the last)
 FROM nginx:alpine
 
+EXPOSE 80
+
 #copy from last phase
 COPY --from=builder /app/build /usr/share/nginx/html
--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello Benku Dora. Cheppu.!
+          Hello Benku Dora. Cheppu. Updated!
         </p>
         <a
           className="App-link"
