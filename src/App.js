28f170b953b80f1a5ae551be40a4c2afe3c0a2bc
Nishan <nishanbeleri@gmail.com>
2021-08-23 00:34:41 +0530

    Refactor names

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello Benku Dora. Cheppu. Updated!
+          Hello Benku Dora. Cheppu ... Litchi!
         </p>
         <a
           className="App-link"
